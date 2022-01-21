3a537680eedd070a9ff843ed06b10b0528f7d19c
Guo <espana1210@gmail.com>
2022-01-21 13:57:51 -0800

    exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+git checkout -b branchname
+fast forwards can only happen if there have not been commits on the original branch 
+while the new branch is being worked on
+git checkout branchname
+git branch -d branchname
+when Git can not determine what file or folder to choose when merging since there have been 
+different commits with changes to the same file
+*/
